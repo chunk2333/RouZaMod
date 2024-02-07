fbf93c5a24f4352a380441ed14874edd36780168
chunk2333 <chunk2333@gmail.com>
2024-02-07 11:47:59 +0800

    加强怪物：邪教徒。 Kakaa在第2/3次攻击会给予易伤/虚弱持续一回合，并且修改其意图图标。增加5点血量。

--- src/main/java/RouZaMod/RouZaMod.java
+++ src/main/java/RouZaMod/RouZaMod.java
@@ -1,4 +1,17 @@
 package RouZaMod;
 
+import com.evacipated.cardcrawl.modthespire.lib.SpireInitializer;
+
+@SpireInitializer
 public class RouZaMod {
+
+    private static final String modID = "RouZa";
+
+    public static String makeModID(String name){
+        return modID + ":" + name;
+    }
+
+    public static void initialize() {
+        new RouZaMod();
+    }
 }
---
+++ src/main/java/monsters/exordium/CultistPatch.java
@@ -0,0 +1,54 @@
+package monsters.exordium;
+//邪教徒，第2/3次攻击会给予易伤/虚弱持续一回合，并且修改其意图图标。增加5点血量。
+import com.evacipated.cardcrawl.modthespire.lib.SpireInsertPatch;
+import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
+import com.evacipated.cardcrawl.modthespire.lib.SpirePostfixPatch;
+import com.evacipated.cardcrawl.modthespire.lib.SpireReturn;
+import com.megacrit.cardcrawl.actions.GameActionManager;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.monsters.exordium.Cultist;
+import com.megacrit.cardcrawl.powers.VulnerablePower;
+import com.megacrit.cardcrawl.powers.WeakPower;
+
+public class CultistPatch {
+
+    @SpirePatch(clz = Cultist.class, method = SpirePatch.CONSTRUCTOR, paramtypez = {float.class, float.class, boolean.class})
+    public static class CultistPatchPostFix{
+        @SpirePostfixPatch
+        public static void PostFix(Cultist __instance){
+            __instance.maxHealth += 5;
+            __instance.currentHealth = __instance.maxHealth;
+        }
+    }
+
+
+
+    @SpirePatch(clz = Cultist.class, method = "takeTurn")
+    public static class CultistPatchInsertFix{
+        @SpireInsertPatch(loc = 106)
+        public static void Insert(Cultist __instance){
+            if(GameActionManager.turn == 3){
+                AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(AbstractDungeon.player, __instance, new VulnerablePower(AbstractDungeon.player, 1, true), 1));
+            }
+
+            if(GameActionManager.turn == 4){
+                AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(AbstractDungeon.player, __instance, new WeakPower(AbstractDungeon.player, 1, true), 1));
+            }
+
+        }
+    }
+
+    @SpirePatch(clz = Cultist.class, method = "getMove")
+    public static class CultistPatchInsertFix1{
+        @SpireInsertPatch(loc = 162)
+        public static SpireReturn<Void> Insert(Cultist __instance){
+            if(GameActionManager.turn == 2 || GameActionManager.turn == 3){
+                __instance.setMove((byte)1, AbstractMonster.Intent.ATTACK_DEBUFF, (__instance.damage.get(0)).base);
+                return SpireReturn.Return();
+            }
+            return SpireReturn.Continue();
+        }
+    }
+}

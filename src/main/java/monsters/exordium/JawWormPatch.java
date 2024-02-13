218fc56899331796057be9fd6b2b62e847fad165
chunk2333 <chunk2333@gmail.com>
2024-02-13 10:44:29 +0800

    修复了一个bug，该bug曾导致大颚虫开局给予脆弱而不是第一次攻击后。

--- src/main/java/monsters/exordium/JawWormPatch.java
+++ src/main/java/monsters/exordium/JawWormPatch.java
@@ -1,10 +1,10 @@
 package monsters.exordium;
 //大颚虫，加1血，首次攻击给予3层脆弱。
-import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
-import com.evacipated.cardcrawl.modthespire.lib.SpirePostfixPatch;
-import com.evacipated.cardcrawl.modthespire.lib.SpirePrefixPatch;
+import com.evacipated.cardcrawl.modthespire.lib.*;
+import com.megacrit.cardcrawl.actions.GameActionManager;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.city.Champ;
 import com.megacrit.cardcrawl.monsters.exordium.JawWorm;
 import com.megacrit.cardcrawl.powers.FrailPower;
 
@@ -18,11 +18,21 @@ public class JawWormPatch {
         }
     }
 
-    @SpirePatch(clz = JawWorm.class, method = "usePreBattleAction")
-    public static class usePreBattleAction{
+//    @SpirePatch(clz = JawWorm.class, method = "usePreBattleAction")
+//    public static class usePreBattleAction{
+//        @SpirePrefixPatch
+//        public static void PreFix(JawWorm __instance){
+//            AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(AbstractDungeon.player, __instance, new FrailPower(AbstractDungeon.player, 3, true), 3));
+//        }
+//    }
+
+    @SpirePatch(clz = JawWorm.class, method = "takeTurn")
+    public static class takeTurnFix{
         @SpirePrefixPatch
         public static void PreFix(JawWorm __instance){
-            AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(AbstractDungeon.player, __instance, new FrailPower(AbstractDungeon.player, 3, true), 3));
+            if (GameActionManager.turn == 1){
+                AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(AbstractDungeon.player, __instance, new FrailPower(AbstractDungeon.player, 3, true), 3));
+            }
         }
     }
 
